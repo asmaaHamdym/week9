8d71b7742070bbf82179fb5837fa142f0e73db4e
asmaaHamdym <asmahamdym@gmail.com>
2024-06-02 22:58:47 +0300

    added useEffect hook to fetch data for each resource

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [resource, setResource] = useState();
-
+  const [resource, setResource] = useState("posts");
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, [resource]);
   return (
     <>
       <div className="App">
         <section className="selection">
-          <button onClick={() => setResource("posts")}>Posts</button>
-          <button onClick={() => setResource("users")}>Users</button>
-          <button onClick={() => setResource("posts")}>Posts</button>
+          <button onClick={() => setResource("Posts")}>Posts</button>
+          <button onClick={() => setResource("Users")}>Users</button>
+          <button onClick={() => setResource("Comments")}>Comments</button>
         </section>
       </div>
       <h1>{resource}</h1>
+      {items.map((item) => {
+        return <div key={item.id}>{item.body || item.name}</div>;
+      })}
     </>
   );
 }
